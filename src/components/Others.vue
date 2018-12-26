2244490075b13b1a5ee014964581adc806e5a5a6
Jeffery Ho <jefferyho1993@outlook.com>
2018-12-26 18:00:14 +0800

    补充说明 开发

--- src/components/Others.vue
+++ src/components/Others.vue
@@ -16,15 +16,15 @@
         </div>
         <div class="other">
             <mu-container class="button-wrapper">
-                <mu-button color="primary" small @click='setBar(0)'>jQuery</mu-button>
-                <mu-button color="secondary" small @click='setBar(1)'>Bootstrap</mu-button>
-                <mu-button color="success" small @click='setBar(2)'>echarts</mu-button>
-                <mu-button color="warning" small @click='setBar(3)'>Baidu Map</mu-button>
-                <mu-button color="secondary" small @click='setBar(4)'>jstree</mu-button>
-                <mu-button color="primary" small @click='setBar(5)'>vue</mu-button>
-                <mu-button color="secondary" small @click='setBar(6)'>iview</mu-button>
-                <mu-button color="success" small @click='setBar(7)'>react</mu-button>
-                <mu-button color="warning" small @click='setBar(8)'>antd</mu-button>
+                <mu-button color="lightBlue900" small @click='setBar(0)'>jQuery</mu-button>
+                <mu-button color="red900" small @click='setBar(1)'>echarts</mu-button>
+                <mu-button color="deepPurple900" small @click='setBar(2)'>Bootstrap</mu-button>
+                <mu-button color="red700" small @click='setBar(3)'>Baidu Map</mu-button>
+                <mu-button color="orange900" small @click='setBar(4)'>jstree</mu-button>
+                <mu-button color="green700" small @click='setBar(5)'>vue</mu-button>
+                <mu-button color="lightBlue500" small @click='setBar(6)'>iview</mu-button>
+                <mu-button color="cyan300" small @click='setBar(7)'>react</mu-button>
+                <mu-button color="primary" small @click='setBar(8)'>antd</mu-button>
             </mu-container>
         </div>
         <div class="charts">
@@ -37,13 +37,15 @@
 var echarts = require('echarts/lib/echarts');
 // 引入柱状图
 require('echarts/lib/chart/pie');
+require('echarts/lib/component/title');
+const colors = ['#01579b', '#b71c1c', '#311b92', '#F44336', '#e65100', '#4FBD8C', '#0E6EC4', '#61DAFB', '#2F52E9'];
 const list = ['jQuery', 'Bootstrap', 'echarts', 'Baidu Map', 'jstree', 'vue', 'iview', 'react', 'antd'];
 export default {
     name: 'Others',
     data () {
         return {
             basic: [80, 60, 45],
-            other: [76, 76, 76, 76, 76, 76, 76, 76, 76],
+            other: [76, 85, 60, 65, 38, 80, 37, 60, 28],
             currentIndex: 0,
             chart: null
         }
@@ -58,16 +60,74 @@ export default {
             }
         },
         drawChart () {
-            let name = list[this.currentIndex], value = this.other[this.currentIndex]; 
+            let name = list[this.currentIndex], value = this.other[this.currentIndex], color = colors[this.currentIndex]; 
+            console.log(name);
             this.chart.setOption({
+                title: {
+                    text: `${value}%`,
+                    subText: '哈哈哈哈',
+                    left: 'center',
+                    top: 'center',
+                    textStyle: {
+                        color: '#333',
+                        fontSize: 20,
+                        fontFamily: 'DINAlternate-Bold'
+                    },
+                    subtextStyle: {
+                        color: '#fff',
+                        fontSize: 30,
+                        fontFamily: 'DINAlternate-Bold',
+                    },
+                    itemGap: -4
+                },
                 series: [
                     {
                         type: 'pie',
-                        name: '',
+                        name: 'pie',
+                        radius: ['70%', '90%'],
+                        itemStyle: {
+                            normal: {
+                                label: {
+                                    show: false
+                                },
+                                labelLine: {
+                                    show: false
+                                }
+                            }
+                        },
+                        hoverAnimation: false,
                         data: [
                             {
                                 name,
-                                value
+                                value,
+                                itemStyle: {
+                                    normal: {
+                                        color: color,
+                                        label: {
+                                            show: false
+                                        },
+                                        labelLine: {
+                                            show: false
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                name: 'gap',
+                                value: 100 - value,
+                                itemStyle: {
+                                    normal: {
+                                        label: {
+                                            show: false
+                                        },
+                                        labelLine: {
+                                            show: false
+                                        },
+                                        color: 'rgba(0, 0, 0, 0.1)',
+                                        borderColor: 'rgba(0, 0, 0, 0)',
+                                        borderWidth: 0
+                                    }
+                                }
                             }
                         ]
                     }
@@ -78,6 +138,15 @@ export default {
     mounted () {
         this.chart = echarts.init(document.getElementById('pie'));
         this.drawChart();
+
+        window.addEventListener('resize', () => {
+            this.chart.resize();
+        })
+    },
+    beforeDestroy () {
+        window.removeEventListener('resize', () => {
+            this.chart.resize();
+        });
     }
 }
 </script>
@@ -114,10 +183,13 @@ export default {
 .charts {
     width: 80%;
     margin: 1.5rem auto;
-    height: 8rem;
+    height: 10rem;
 }
 .pie{
     width: 100%;
     height: 100%;
+}
+.btn-lang{
+    display: none;
 }
 </style>
